da1f6786be8244f0017229bc23b627455fb52de7
burcineren <burcin.ern969@gmail.com>
2024-12-25 23:11:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+        <MoviesGrid />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
